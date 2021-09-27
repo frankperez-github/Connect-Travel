0605eb3f6426fda388df240c5aa604572115ae28
Frank Pérez <fp848584@gmail.com>
2021-09-27 17:58:53 -0400

    Front End Changes

--- pages/AboutUs.jsx
+++ pages/AboutUs.jsx
@@ -3,7 +3,7 @@ import RedSection from "./Components/RedSection";
 function About_Us(){
     return(
         <div className="AboutUs redSection">
-            <h1 className="AboutTitle">About Connect</h1>
+            <h1 className="AboutTitle">About CONNECT</h1>
 
             <p className="AboutUsP">As a family Travel Agency we are commited to making our clients feels an excellent experience before, during and after their trips; thus fulfilling our objective; please them.</p>
 
