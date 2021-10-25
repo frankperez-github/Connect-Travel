a6bc3e45d2832b9111cc675d5a5f7b5ddfabf697
Adiazx14 <adrianvaldes1439@gmail.com>
2021-10-24 23:43:50 -0400

    fixed links

--- Components/RedSection.jsx
+++ Components/RedSection.jsx
@@ -9,31 +9,31 @@ function RedSection (){
 
             <div className="Destinations">
 
-                <Link href="/Destination">
+                <Link passHref={true} href="/Region/1">
                     <div className="SpecificDestination">
                         <Image alt="Destiny" width={1000} height={1000} src="/South_America.svg" />
                     </div>
                 </Link>
 
-                <Link href="/Destination">
+                <Link passHref={true} href="/Region/2">
                     <div className="SpecificDestination">
                     <Image alt="Destiny" width={1000} height={1000} src="/Caribbean.svg" />
                     </div>
                 </Link>
 
-                <Link href="/Destination">
+                <Link passHref={true} href="/Region/3">
                     <div className="SpecificDestination">
                     <Image alt="Destiny" width={1000} height={1000} src="/Mexico_City.svg" />
                     </div>
                 </Link>
 
-                <Link href="/Destination">
+                <Link passHref={true} href="/Region/4">
                     <div className="SpecificDestination">
                     <Image alt="Destiny" width={1000} height={1000} src="/Europe.svg" />
                     </div>
                 </Link>
 
-                <Link href="/Destination">
+                <Link passHref={true} href="/Region/5">
                     <div className="SpecificDestination">
                     <Image alt="Destiny" width={1000} height={1000} src="/Asia.svg" />
                     </div>
