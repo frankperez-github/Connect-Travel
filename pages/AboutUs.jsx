ef3d1194a879cad36ad9340ab934ffbaa7d126a1
Frank Perez <fp848584@gmail.com>
2022-02-22 23:04:07 -0500

    changes in aboutUs

--- pages/AboutUs.jsx
+++ pages/AboutUs.jsx
@@ -1,22 +1,34 @@
+import Image from 'next/image'
 import DestinationCard from '../Components/DestinationCard'
 
 function About_Us({regions}){
     return(
         <div className="AboutUs">
-            <h1 className="AboutTitle">About CONNECT</h1>
-
-            <p className="AboutUsP">As a family Travel Agency we are commited to making our clients feels an excellent experience before, during and after their trips; thus fulfilling our objective; please them.</p>
-
-            <button className="ConnectButton ContactButton">Contact</button>
-
-            <div className="redSection">
-
-                <h1>Find your next destination</h1>
-
-                <div className='DestinationRow'>
-                {regions.map(pack=><DestinationCard region={pack} key={pack.id}/>)}
-                </div>
+          <div className="PrincipalAbout">
+            <div className="leftAbout">
+              <Image src="/Character-reading.svg" width={270} height={270} />
             </div>
+
+            <div className="rightAbout">
+              <h2 className="AboutTitle Info">About</h2>
+              <h1 className="AboutTitle Info">Connect Travel Corp.</h1>
+
+              <p className="AboutUsP">As a family Travel Agency we are commited to making our clients feels an excellent experience before, during and after their trips; thus fulfilling our objective; please them.</p>
+
+              <button className="ConnectButton ContactButton">Contact</button>
+            </div>            
+            
+          </div>
+
+          <div className="redSection">
+
+            <h1>Find your next destination</h1>
+
+              <div className='DestinationRow'>
+              {regions.map(pack=><DestinationCard region={pack} key={pack.id}/>)}
+              </div>
+          </div>
+
         </div>
     );
 }export default About_Us;
--- pages/Terms_Conditions.jsx
+++ pages/Terms_Conditions.jsx
@@ -15,10 +15,43 @@ function Terms_Conditions(){
             <p>A deposit of $100 per person is required within 2 days (48 hours) of making the reservation. If the total price of the tour is less than $100, the full amount will be collected at time of booking. Payment is not deemed until Connect Travel Corporation receives it. If deposit is not received by due date, the reservation Will automatically be cancelled in the system by 5:00 PM EST.</p>
 
             <h4>B. Balance Due</h4>
-            <p>A deposit of $100 per person is required within 2 days (48 hours) of making the reservation. If the total price of the tour is less than $100, the full amount will be collected at time of booking. Payment is not deemed until Connect Travel Corporation receives it. If deposit is not received by due date, the reservation Will automatically be cancelled in the system by 5:00 PM EST.</p>
+            <p>The balance must be paid in FULL no less than 30 days prior to departure. Inmediate payment in full is required at time of booking for reservations made less than 30 days before departure. (Bookings for travel within Dec. 20 to Dec. 31 will require full payment 60 days prior to departure). WE RESERVE THE RIGHT TO CANCEL THE BOOKING IF BALANCE IS NOT PAID ON TIME. In addition, Connect Travel Corporation reserves the right to impose cancellation charges should payments not be received within the specified periods. Connect Travel Corporation is not responsible for any other travel arrangements affected due to our cancellations. Connect Travel Corporation MUST RECEIVE ALL PAYMENTS before vouchers and other travel documents could be realesed.</p>
 
             <h4>C. Forms of Payment</h4>
-            <p>A deposit of $100 per person is required within 2 days (48 hours) of making the reservation. If the total price of the tour is less than $100, the full amount will be collected at time of booking. Payment is not deemed until Connect Travel Corporation receives it. If deposit is not received by due date, the reservation Will automatically be cancelled in the system by 5:00 PM EST.</p>
+            <p>Payments in Cash must be delivered in person at Connect Travel Corporation office. For credit card payments, we accept Visa, MasterCard, American Express and Discover Card. Please request a Credit Card Authorization Form from your travel agent.</p>
+
+            <h4>NOTE: If payment is being made for a deposit or final payment on due date, credit card is the only acceptable form of payment.</h4>
+
+            <h2>LATE BOOKINGS</h2>
+            <p>Late bookings are considered "rush" orders and require special handling. Therefore late bookings will incur additional costs for faxing, updating and confirming your hotel accommodations and other services required. Bookings made less than 7 days prior to departure will be subject to an additional charge of $25.00</p>
+
+            <h4>Connect Travel Corporation will not accept reservations for travel within 3 days.</h4>
+
+            <h2>TRAVEL DOCUMENTS</h2>
+            <p>It is the sole responsability of the passenger to obtain all necessary travel documents before traveling. Passports must have a minimum validity of 6 months as of date of departure. Visas, vaccination and any other requirements should be obtained directly with the Consulate of Embassy of the countries you are visiting before traveling. Connect Travel Corporation will not be liable for any loss, expense, cost, damage, injury or loss of enjoyment suferred if you cannot travel because you have not complied with such requirements</p>
+
+            <h2>TRAVEL INSURANCE</h2>
+            <p>Connect Travel Corporation highly recommends acquiring travel insurance along with your package. Passengers must contact the Insurance Company in case of emergency. Please keep in mind that preexisting conditions may be an exception with the coverage that the insurance company provides. For any date changes, you may contact Connect Travel Corporation up to two labor days in advance before coverage is in effect.<br/><br/>
+            
+            For any refunds, you must keep all your receipts and contact the Insurance Company<br/><br/>
+            
+            If passenger decides not to get travel insurance, Connect Travel Corporation will not be responsible due to any mishap cancellation, incovenience or suffering, before, during or after the trip. This includes losing right to refund due to any complaints or expenses from accident or illness ocurrence on trip</p>
+
+            <h2>CANCELLATIONS AND MODIFICATIONS</h2>
+            <h4>A. Cancellations Charges/No-Shows</h4>
+            <p>All cancellations arre subject to a $50.00 cancellation charge per package, in addition to the applicable percentage of the total tour price as shown on the chart below. Significant additional penalties (i.e. Non-refundable, and or non-transferable airlines tickets, fees or hotel penalties, etc.) may apply. A written notice of cancellation is required for confirmed reservstions and must be sent directly to Connect Travel Corporation via email <a href="mailto: book@connecttravel.us">book@connecttravel.us</a>. The departure date constitutes the date indicated on Connect Travel Corporation's confirmation. If you are cont present on the day of departure, this will be considered a No-Show and will incur in a 100% cancellation fee.<b/><b/>
+            
+            *The following table of charges will apply when notice of cancellation is given after the booking is confirmed:<b/>
+            ALL CHARGES BELOW ARE PER PERSON.</p>
+
+            <div className="leftColumn">
+                <h4>Notice received by Connect Travel Corporation prior to departure</h4>
+                <p>31 days or more</p>
+                <p>30 - 22 days</p>
+                <p>21 - 8 days</p>
+                <p>7 - 1 days</p>
+                <p>Days of departure/No-Show</p>
+            </div>
 
         </div>
     </div>
